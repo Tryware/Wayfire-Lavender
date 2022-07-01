e0ea7563524c9635b57e334db8c0df863e25378d
Tryware <82564850+Tryware@users.noreply.github.com>
2022-07-01 20:18:44 -0300

    Add files via upload

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,11 @@
+import totallynotmoddeddracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+totallynotmoddeddracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
